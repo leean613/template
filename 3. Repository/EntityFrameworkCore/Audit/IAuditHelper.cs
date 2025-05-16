76d90760968e8bc85df2db6cfac5158608a59059
lyan <lytruongan613@gmail.com>
2025-05-16 12:53:04 +0700

    remove audit

--- 3. Repository/EntityFrameworkCore/Audit/IAuditHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
-
-namespace EntityFrameworkCore.Audits
-{
-    public interface IAuditHelper
-    {
-        void ApplyConcepts(EntityEntry entry);
-    }
-}
